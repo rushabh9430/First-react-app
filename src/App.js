cd99f49087e2e0b0f9c14e816f9bb949b2cfe825
rushabh9430 <sutariyarushabh2@gmail.com>
2022-12-07 20:37:25 +0530

    include P tag

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>
         </p>
+        <p>This is a default react app</p>
       </header>
     </div>
   );
