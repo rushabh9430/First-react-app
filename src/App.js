fc588318803537b28ede11a29946393a64545d57
rushabh9430 <sutariyarushabh2@gmail.com>
2022-12-16 09:19:11 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+
+  let arrys = new Array(1,2,3,4,5)
+  let admin = ( ) => {
+    const a = arrys[4]
+    const b = arrys[3]
+    const c = arrys[2]
+    const d = arrys[1]
+    const e = arrys[0]
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log(e);
+  }
+
+
+  //  This Is Distructuring Concept  
+
+  // 1
+  let [a,b,c,d,e] = [1 , 2 , 3 , 4 ,5]
+  console.log(a , b)
+
+
+  // 2
+  let [x,...rest] = [1 , 2 , 3 , 4 ,5]
+  console.log(rest)
+
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" onClick = {admin}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code>
