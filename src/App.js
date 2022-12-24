399d05708f75241520cc9e26f5f08d67db82a4b9
rushabh9430 <sutariyarushabh2@gmail.com>
2022-12-24 10:45:56 +0530

    StateInClassBase

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
-import { useState } from "react";
+import State from './Components/StateInClassbaseCom'
 import "./App.css";
-
- let data = 
-         {
-          name: "Krishan Mandvani",
-          id: 1056,
-          desc: "This is a test desc !"
-        }
+// ⚛️   Class Base State ⬇️
 function App() {
-        // String
-        const [name, setName] = useState('a');
-        // Number
-        const [age, setAge] = useState(1)
-        // Boolean Values 
-        const [boolean, setBoolean] = useState(true)
-        // Arrays Of Object
-        const [arrays, setArrays] = useState(data)
-        const onclickHandler = ( ) => {
-                // String
-                setName('b')
-                // Number
-                setAge(age + 1)
-                // Boolean Values Single Line Condition 
-                const bool = boolean ? false : true 
-                setBoolean(bool)   
-                // arrays Of Object
-                setArrays({
-                        name : "Piyush Mavani", desc : "This is a new discription Bro !", id : 25
-                })     
-        }
   return (
     <div className="App">
-      <h2>HEllo React y!</h2>
-      <hr />
-      Name  is {name} <br />
-      Click On This Button ⬇️ <br />
-      <hr />
-      Age is {age}
-      <hr />
-      boolean value is {boolean ? 'True' : 'False'} 
-      <br /> <hr />
-      Arrays of Data Properties ➡️ 
-        <br />
-        name is {arrays.name} <br />
-        and Discription of this page is {arrays.desc} And Id {arrays.id}
-        <br /><br />
-      <button onClick={onclickHandler}>Submit</button>
+        <h1>State In Class Base Components</h1>
+        <State />
     </div>
   );
 }
---
+++ src/Components/StateInClassbaseCom.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+export default class State extends Component {
+constructor(props){
+        super(props);
+        this.state = {
+                name : "Ryan Reynolds",
+                age : 39,
+                married : 'yes' ,
+                about : {
+                        Hobbie : 'work',
+                        wife  : 'Scarlett Johansson'
+                }
+        }
+}
+
+        OnclickHandler = () => {
+                this.setState({name : "Dwayne Johnson",age : 25 , married : 'No' , about : {Hobbie : "Dance"}})
+        }
+
+        
+  render() {
+    return (
+      <div>
+                <p>Name is {this.state.name}</p>
+                <p>Age is {this.state.age}</p>
+                <p>Married ?  {this.state.married}</p>
+                <p>Hobbie is {this.state.about.Hobbie}</p>
+                <button className = {'btn_css'} onClick = {this.OnclickHandler}>Update details</button>
+      </div>
+    )
+  }
+}
