1d3fe30fbccbb4eddc81b06b47f05f5b81c47080
rushabh9430 <sutariyarushabh2@gmail.com>
2022-12-24 09:25:21 +0530

    Hook

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
-//   New Including File ⬇️
-import Header from "./Components/header.js";
 
-// Note  :  Add New components File Is About
+ let data = 
+         {
+          name: "Krishan Mandvani",
+          id: 1056,
+          desc: "This is a test desc !"
+        }
 function App() {
-  const Student = [
-    {
-      name: "Rushabh Sutariya",
-      id: 1054
-    },
-    {
-      name: "Abhishekh Gajera",
-      id: 1
-    },
-    {
-      name : "Krunal Dhorajiya",
-      id : 205
-    },
-    {
-      name : 'hardik Chauhan',
-      id : 2506
-    }
-  ];
-
+        // String
+        const [name, setName] = useState('a');
+        // Number
+        const [age, setAge] = useState(1)
+        // Boolean Values 
+        const [boolean, setBoolean] = useState(true)
+        // Arrays Of Object
+        const [arrays, setArrays] = useState(data)
+        const onclickHandler = ( ) => {
+                // String
+                setName('b')
+                // Number
+                setAge(age + 1)
+                // Boolean Values Single Line Condition 
+                const bool = boolean ? false : true 
+                setBoolean(bool)   
+                // arrays Of Object
+                setArrays({
+                        name : "Piyush Mavani", desc : "This is a new discription Bro !", id : 25
+                })     
+        }
   return (
     <div className="App">
-      {/* Pass velue */}
-      <Header Student={Student} />
+      <h2>HEllo React y!</h2>
+      <hr />
+      Name  is {name} <br />
+      Click On This Button ⬇️ <br />
+      <hr />
+      Age is {age}
+      <hr />
+      boolean value is {boolean ? 'True' : 'False'} 
+      <br /> <hr />
+      Arrays of Data Properties ➡️ 
+        <br />
+        name is {arrays.name} <br />
+        and Discription of this page is {arrays.desc} And Id {arrays.id}
+        <br /><br />
+      <button onClick={onclickHandler}>Submit</button>
     </div>
   );
 }
-
 export default App;
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,20 +1,20 @@
 
 //  🔯 First Method  ⬇️ Without using Props
-import React from 'react'
-import Contact from './Contact'
+// import React from 'react'
+// import Contact from './Contact'
 
-//  🔯 How to access Header Components Value in Contact Components⬇️
-function header(props) {
-        // console.log(props.Student)
-        return(
-                <div>
-                        <Contact value = {props.Student}  />
-                        <h1> Header File : 1St Object Name is {props.Student[1].name}</h1>
-                </div>
-        )
-}
+// //  🔯 How to access Header Components Value in Contact Components⬇️
+// function header(props) {
+//         // console.log(props.Student)
+//         return(
+//                 <div>
+//                         <Contact value = {props.Student}  />
+//                         <h1> Header File : 1St Object Name is {props.Student[1].name}</h1>
+//                 </div>
+//         )
+// }
 
-export default header
+// export default header
 
 
 
