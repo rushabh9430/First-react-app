a180a0f351679ba18019ab0440f90afc78420298
rushabh9430 <sutariyarushabh2@gmail.com>
2022-12-28 16:22:24 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,64 +1,40 @@
-import React from "react";
-import { useState ,useEffect} from "react";
+import React, { Component } from "react";
 
-// 📝Note : jyare tamre page reload thai and changes karvo hoi tyare useEffect no use thai chhe Bassicly te sadi rite pan SetName no use kari ne pan tahi to tene Function onclick ma farjiyat levu pade like last ma batavel chhe
+export default class App extends Component {
+  componentWillMount() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Rushabh Sutariya",
+      count: 1,
+    };
+  }
 
-// [] => Empty dependency
-// birth of component => Mounting
+  componentDidUpdate(prevProps, prevState) {
+    console.info("ComponentDidUpdate");
+    if (prevState.count !== this.state.count) {
+      this.setState({ name : "Pravin Sutariya" });
+    }
+  }
 
-// ⚛️ 1.Mounting Dom reload thai ne Action run thai
+//   componentWillUnmount() {
+//     alert("ComponentWillUnmout => PageLeave");
+//   }
 
-// export default function App() {
-//     const [name, setName] = useState("adam")
-//     useEffect(() => {
-//         setName("Pravin")
-//     }, [])
-//   return (
-//     <div>
-//          name is {name}
-//     </div>
-//   )
-// }
+  onClickHandler = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
-
-// ⚛️2.Updating
-// export default function App() {
-//   const [name, setName] = useState("Rushabh");
-//     useEffect(() => {
-//     }, [name])
-//   return (
-//     <div>
-//         name is {name}
-//       <button onClick={()=>setName("Krunal")}>Update</button>
-//     </div>
-//   );
-// }
-
-// ⚛️ 3.unMounting => screen remove 
-// export default function App() {
-
-//   useEffect(() => {
-  
-//     return () => {
-//         alert("You want to exit this tab !")
-//     }
-//   }, [])
-//   return (
-//     <div>
-      
-//     </div>
-//   )
-// }
-
-
-// Avi rite na thai etle apde tene oclick par levu pade button par function muki click kari ne change thai did you Understood bro rushabh!
-export default function App() {
-const [name, setName] = useState("adam")
-
- setName("Pravin")
-  return (
-    <div>
-         name is {name}
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        name is {this.state.name}
+        <br />
+        Count {this.state.count}
+        <button onClick={this.onClickHandler}>Click Me</button>
+      </div>
+    );
+  }
 }
