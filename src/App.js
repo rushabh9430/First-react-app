1153b82f777c0a80843260db4adb224aa33414d6
rushabh9430 <sutariyarushabh2@gmail.com>
2022-12-30 06:33:37 +0530

    when you click the button and show alert do you want to leave this page

--- src/App.js
+++ src/App.js
@@ -1,72 +1,35 @@
-//  ! Method - 1 => how to work componentDidUpdate with states
-// import React, { Component ,setState } from "react";
-// export default class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       name: "rushabh sutariya",
-//       count: 2
-//     };
-//   }
-
-// //   * How to work in state
-//   componentDidUpdate(prevProps , prevState ) {
-//     if(prevState.count !== this.state.count)
-//     {
-//         this.setState({name : "pravin"})
-//     }
-//   }
-
-//   onclickHandler = () => {
-//     this.setState({count : this.state.count + 1})
-//   };
-
-//   render() {
-//     return (
-//       <div className="App">
-//         count {this.state.count}
-//         <button onClick={this.onclickHandler}>onclick</button>
-//       </div>
-//     );
-//   }
-// }
-
-//  ! Method - 2 => how to work componentDidUpdate with props
-import React, { Component, setState } from "react";
-
+import React , { Component , setState } from 'react'
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "rushabh sutariya",
-      count: 1,
-      isValid: true,
-    };
-  }
 
-  onclickHandler = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  render() {
-    return (
-      <div className="App">
-        name is {this.state.name}
-        <Title component1 = {this.state.count}/>
-        <button onClick={this.onclickHandler}>submit</button>
-      </div>
-    );
-  }
+    constructor(){
+        super()
+        this.state = {
+            isvalid : true
+        }
+    }
+    clickme = () => {
+        this.setState({isvalid : !this.state.isvalid}) 
+    }
+    render(){
+
+        return(
+            <div>
+                isvalid{this.state.isvalid ? 'yes' : "no"}
+                {this.state.isvalid && <Home />}
+                <button onClick={this.clickme}>submit</button>
+            </div>
+        )
+    }
 }
 
-class Title extends Component {
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps !== this.props.count) {
-      console.log("componentDidUpdate");
+class Home extends Component{
+
+    componentWillUnmount(){
+        alert("are you sure exit this page ?")
     }
-  }
-  render() {
-    return <div className="Title">
-        count are {this.props.component1}
-        </div>;
-  }
+    render(){
+        return(
+            <div>Home</div>
+        )
+    }
 }
