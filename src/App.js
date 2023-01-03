cdf91030f3adbf4d2b74abb3ae17e996e3e3b6ee
rushabh9430 <sutariyarushabh2@gmail.com>
2023-01-03 09:00:46 +0530

    Timer_classComponent

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import React from "react";
-// react - rounter - dom is external package so first you install 
-// command is => npm i react-router-dom
-// and rap out < BrowserRouter> <App /> <BrowserRouter /> in index.js  
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
-import About from "./Components/about";
-import Contact from "./Components/Contact";
+import React, { Component } from "react";
 
-export default function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/Contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+  }
+
+  componentDidMount(){
+    setInterval(() => {
+        this.setState({data : new Date().toLocaleTimeString()})
+    }, 1000);
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.data}
+      </div>
+    );
+  }
 }
